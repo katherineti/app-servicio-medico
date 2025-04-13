1f92d10f2a77cfd0595db4a5c391ba9b8c3c64ab
katherineti <katherineti1609@gmail.com>
2025-04-13 17:06:00 -0400

    dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../material/material.module';
+import { FeatherIconsModule } from '../feathericons/feathericons.module';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MaterialModule],
+  imports: [MaterialModule
+    , FeatherIconsModule
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
